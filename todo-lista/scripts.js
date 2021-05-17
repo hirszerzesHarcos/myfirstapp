ea93fc86effa2a854f0550889eb3f2fd259e1cc5
hirszerzesHarcos <hirszerzesharcos@gmail.com>
2021-05-17 06:53:30 +0200

    formating is undergoing

--- todo-lista/scripts.js
+++ todo-lista/scripts.js
@@ -1,16 +1,16 @@
 $('input[type="button"]').on("click", addToDo);
+$("li").on("click", lineThrough);
 $('input[type="text"]').keypress(isEnter);
 
 function isEnter(e) {
   if(e.key=="Enter"){
-    console.log("hahaha");
     $('input[type="button"]').click();
     return false;
   }
 }
 
 function addToDo() {
-  $("ul").append("<li>" + $('input[type="text"]').val() + "</li>");
+  $("ul").append(`<li>${$('input[type="text"]').val()}</li>`);
   $("li").on("click", lineThrough);
 }
 
