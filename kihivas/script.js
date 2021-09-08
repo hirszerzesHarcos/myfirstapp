0c1aac9d8d0c8b827e18ea13c361f2e22ca4df1c
hirszerzesHarcos <hirszerzesharcos@gmail.com>
2021-09-08 18:35:50 +0200

    Érvényesítés folytatva.

--- kihivas/script.js
+++ kihivas/script.js
@@ -1,59 +1,31 @@
 function start() {
-  const inputs = document.querySelectorAll('input');
+  form = document.querySelector("form");
+  lastName = document.getElementById("lastName");
+  firstName = document.getElementById("firstName");
+  age = document.getElementById("age");
+  inputs.push(lastName, firstName, age);
+  greeting = document.getElementById("greeting");
+  ageAnalysis = document.getElementById("ageAnalysis");
+  buttons.push(greeting, ageAnalysis);
   inputs.forEach(inputValidation);
-  /*  age = document.getElementsByTagName("input")[2];
-    ul = document.getElementsByTagName("ul")[0];
-    age.setCustomValidity("<li>Nem lesz jó a kor.</li>");
-    ul.append(age.validationMessage);
-    ul.append(age.reportValidity());
-    age.addEventListener("input", val);*/
-
 }
-
-/*function val() {
-  ul.append(age.checkValidity());
-}*/
-
-/*const nameInput = document.querySelector('input');
-
-nameInput.addEventListener('input', () => {
-  nameInput.setCustomValidity('');
-  nameInput.checkValidity();
-});
-
-nameInput.addEventListener('invalid', () => {
-  if (nameInput.value === '') {
-    nameInput.setCustomValidity('Enter your username!');
-  } else {
-    nameInput.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
-  }
-});*/
-
-
-
-/*const form = document.querySelector('form');*/
-
 
 function inputValidation(input) {
   input.addEventListener("input", validityCheck);
 }
 
 function validityCheck(event) {
+  console.log(event.target.value);
   console.log(event.target.validity.valid);
+  console.log(event.target.checkValidity());
+  if (form.checkValidity()) {
+    console.log(age.value);
+  }
 }
-/*input.oninvalid = (e) => {
-  document.getElementById(`${e.target.id}-error`).removeAttribute('hidden');
-}
-})form.onsubmit = (e) => {
-console.log(e);
-e.preventDefault();
-}
-numbers.forEach(myFunction);
 
-function myFunction(item) {
-sum += item;
-}*/
-
+var form, lastName, firstName, age, greeting, ageAnalysis;
+const inputs = [];
+const buttons = [];
 window.addEventListener("load", start);
 
 // function myFunction() {
@@ -68,49 +40,3 @@ window.addEventListener("load", start);
 //   }
 //   document.getElementById("demo").innerHTML = text;
 // }
-
-// <form>
-//   <label for="choose">Would you prefer a banana or a cherry?</label>
-//   <input id="choose" name="i_like" required pattern="[Bb]anana|[Cc]herry">
-//     <button>Submit</button>
-// </form>
-
-//   form {
-//     font: 1em sans-serif;
-//   max-width: 320px;
-// }
-
-// p > label {
-//     display: block;
-// }
-
-//   input[type="text"],
-//   input[type="email"],
-//   input[type="number"],
-//   textarea,
-//   fieldset {
-//     width : 100%;
-//   border: 1px solid #333;
-//   box-sizing: border-box;
-// }
-
-//   input:invalid {
-//     box - shadow: 0 0 5px 1px red;
-// }
-
-//   input:focus:invalid {
-//     box - shadow: none;
-// }
-
-
-
-//   <form novalidate>
-//     <p>
-//       <label for="mail">
-//         <span>Please enter an email address:</span>
-//         <input type="email" id="mail" name="mail" required minlength="8">
-//           <span class="error" aria-live="polite"></span>
-//     </label>
-//   </p>
-//       <button>Submit</button>
-// </form>
